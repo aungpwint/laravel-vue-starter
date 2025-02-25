57e1ab51ef59cd5af6f19dfb14ac24655209a983
Aung Pwint <aungpwintofficial@gmail.com>
2025-02-25 15:00:04 +0630

    init

---
+++ __unconfig_vite.config.ts
@@ -0,0 +1,39 @@
+
+let __unconfig_data;
+let __unconfig_stub = function (data = {}) { __unconfig_data = data };
+__unconfig_stub.default = (data = {}) => { __unconfig_data = data };
+import vue from '@vitejs/plugin-vue';
+import autoprefixer from 'autoprefixer';
+import laravel from 'laravel-vite-plugin';
+import path from 'path';
+import tailwindcss from 'tailwindcss';
+import { defineConfig } from 'vite';
+
+const __unconfig_default =  defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/js/app.ts'],
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './resources/js'),
+        },
+    },
+    css: {
+        postcss: {
+            plugins: [tailwindcss, autoprefixer],
+        },
+    },
+});
+
+if (typeof __unconfig_default === "function") __unconfig_default(...[{"command":"serve","mode":"development"}]);export default __unconfig_data;
